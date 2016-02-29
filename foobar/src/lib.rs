9134c43af8ca9774e55c35b69b16b6907bd635dd
BourgondAries <macocio@gmail.com>
2016-02-29 03:55:59 +0100

    Implement a counter

--- foobar/src/lib.rs
+++ foobar/src/lib.rs
@@ -1,3 +1,35 @@
+use std::io;
+
+struct Exact {
+	a: i32,
+	b: i32,
+}
+
 pub fn it_works() {
 	println!("It works inside!");
+}
+
+pub fn ok() -> i64 {
+	let x = Exact { a: 100, b: -399 };
+	let mut variable = 100_000_000i64;
+	let reader = io::stdin();
+	let mut line = String::new();
+	reader.read_line(&mut line).ok().expect("Failed");
+	let lint: Option<i64> = line.trim().parse::<i64>().ok();
+	{
+		let local = &mut variable;
+		*local += 10;
+		for i in 1..134_000 {
+			*local += 3 * i;
+		}
+	}
+	let f = match lint {
+		Some(v) => v,
+		None => {
+			println!("Could not parse.");
+			0
+		},
+	};
+	println!("{}", line);
+	variable + (x.a as i64) + (x.b as i64) + f
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,31 @@ extern crate foobar;
 mod hello;
 
 fn main() {
-	println!("Hey!");
-	foobar::it_works();
-	hello::print_hello();
+	println!("Enter numbers, enter zero or c-d to exit.");
+	let mut reader = std::io::stdin();
+	let mut sum = 0i64;
+	let mut line = String::from("0");
+
+	while line != "".to_string() {
+		line = String::new();
+		reader.read_line(&mut line)
+			.ok()
+			.expect("Failed to read line");
+		let value: Option<i64> = line.trim().parse::<i64>().ok();
+		println!("Line: {}", line);
+		let integer = match value {
+			Some(x) => {
+				println!("Not continue");
+				x
+			},
+			None => {
+				println!("Continue");
+				continue;
+			},
+		};
+		println!("Hey!");
+		sum += integer;
+	}
+	println!("{}", sum);
+	return;
 }
