ab0b0345d1ce035e9a75485d461c26754e4c653f
BourgondAries <macocio@gmail.com>
2016-03-03 19:46:24 +0100

    Test logging functionality

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,43 @@
 extern crate foobar;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 mod hello;
 
+struct MyErr {
+	message: String,
+}
+
+impl MyErr {
+	fn to_string(&self) -> &String {
+		&self.message
+	}
+}
+fn fbor<'a>() {
+	let x = 12;
+	let y: &'a i32 = &x;
+}
+
+fn tess(a: i64) {
+	fbor();
+	println!("{}", a);
+}
+fn tropp() {
+	env_logger::init().unwrap();
+	info!(target: "Cool", "Starting tropp");
+	tess(100);
+	warn!("Commencing variable");
+	let a: Result<i64, String> = Ok(1);
+	let b: Result<i64, MyErr> = Err(MyErr {message: "Wow!".to_string()});
+	match b {
+		Err(mine) => println!("My error: {}", mine.to_string()),
+		_ => println!("Totally OK"),
+	}
+}
+
 fn main() {
+	tropp();
+	return;
 	let temp = hello::world(504, "/", 2);
 	match temp {
 		Ok(value) => println!("Computed: {}", value),
