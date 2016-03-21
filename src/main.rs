eb3e02fd457e61b0e0fab13222d1e949c13aacd5
Bourgond Aries <macocio@gmail.com>
2016-03-21 06:36:28 +0100

    Add some simple shading. Improve the toml reader

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,84 @@
 #[macro_use]
 extern crate glium;
 extern crate glm;
+extern crate time;
 extern crate toml;
 
 mod toml_graphics;
 
 fn main() {
 	use glium::{DisplayBuild, Surface};
+	use time::PreciseTime;
 	loop {
-		let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
-		let vertex_buffer;
+		let display = glium::glutin::WindowBuilder::new().with_depth_buffer(24).build_glium().unwrap();
 
-		{
-			vertex_buffer = toml_graphics::read_toml(&display, "models/pony_model_applejack/aj_full.toml");
-			// vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-		}
+		let begin = PreciseTime::now();
+		let (vertex_buffer, normal_buffer, index_buffer) = toml_graphics::read_toml(&display, "models/test/box.toml");
+		let end = PreciseTime::now();
+		println!("Loading model took {:?}", begin.to(end));
 
-		let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+		let indices = index_buffer; // glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
 		let vertex_shader_src = r#"
-			#version 140
+			#version 150
 
 			uniform mat4 transformation;
 			in vec3 position;
+			in vec3 normal;
+			out vec3 v_normal;
 
 			void main() {
+				v_normal = transpose(inverse(mat3(transformation))) * normal;
 				gl_Position = transformation * vec4(position, 1.0);
 			}
 		"#;
 
 		let fragment_shader_src = r#"
-			#version 140
+			#version 150
 
+			in vec3 v_normal;
 			out vec4 color;
 
 			void main() {
-				color = vec4(1.0, 0.0, 0.0, 1.0);
+				vec3 u_light = vec3(1.0, 0.0, 0.0);
+				float brightness = dot(normalize(v_normal), normalize(u_light));
+				vec3 dark_color = vec3(0.6, 0.0, 0.0);
+				vec3 regular_color = vec3(1.0, 0.0, 0.0);
+				color = vec4(mix(dark_color, vec3(1.0, 0.0, 0.0), brightness), 1.0);
 			}
 		"#;
 
 		let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
 		let mut t: f32 = -0.5;
+
+		let params = glium::DrawParameters {
+			depth: glium::Depth {
+				test: glium::draw_parameters::DepthTest::IfLess,
+				write: true,
+				.. Default::default()
+			},
+			.. Default::default()
+		};
+
+
 		'displayer: loop {
-			t += 1e-3;
-			if t > 0.5 {
-				t = -0.5;
+			t += 0.01;
+			if t > 2.*std::f32::consts::PI {
+				t = 0.0;
 			}
 			let mut target = display.draw();
-			target.clear_color(0.0, 0.0, 1.0, 1.0);
-			let sca = 0.2f32;
+			target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
 			let uniform = uniform![
 				transformation: [
-					[sca, 0.0, 0.0, 0.0],
-					[0.0, sca, 0.0, 0.0],
-					[1.0, 0.0, sca, 0.0],
-					[t, 0.0, t, 1.0f32],
+					[t.cos(), -t.sin(), 0.0, 0.0],
+					[t.sin(), t.cos(), 0.0, 0.0],
+					[1.0, 0.0, 1.0, 0.0],
+					[0.0, 0.0, 0.0, 1.0f32],
 				]
 			];
-			target.draw(&vertex_buffer, &indices, &program, &uniform,
-						&Default::default()).unwrap();
+			target.draw((&vertex_buffer, &normal_buffer), &indices, &program, &uniform,
+						&params).unwrap();
 			target.finish().unwrap();
 
 			for ev in display.poll_events() {
--- src/toml_graphics.rs
+++ src/toml_graphics.rs
@@ -1,16 +1,20 @@
 extern crate glium;
 extern crate toml;
 use glium::backend::Facade;
-use glium::VertexBuffer;
+use glium::{IndexBuffer, VertexBuffer};
 
 #[derive(Copy, Clone)]
 pub struct Vertex {
 	position: [f32; 3],
 }
-
+#[derive(Copy, Clone)]
+pub struct Normal {
+	normal: [f32; 3],
+}
 implement_vertex!(Vertex, position);
+implement_vertex!(Normal, normal);
 
-pub fn read_toml(display: &Facade, filename: &str) -> VertexBuffer<f32> {
+pub fn read_toml<F>(display: &F, filename: &str) -> (VertexBuffer<Vertex>, VertexBuffer<Normal>, IndexBuffer<u32>) where F: Facade {
 	use std::io::Read;
 	use std::fs::File;
 
@@ -27,10 +31,73 @@ pub fn read_toml(display: &Facade, filename: &str) -> VertexBuffer<f32> {
 			panic!();
 		}
 	};
-	create_vertex_buffer(&display, &toml)
+	(create_vertex_buffer(display, &toml),
+	create_normal_buffer(display, &toml),
+	create_face_buffer(display, &toml))
 }
 
-fn create_vertex_buffer(display: &Facade, table: &toml::Table) -> VertexBuffer<f32> {
+fn create_normal_buffer<F>(display: &F, table: &toml::Table) -> VertexBuffer<Normal> where F: Facade {
+	use toml::Value;
+	let toml = match table["object"] {
+		Value::Array(ref array) => array,
+		_ => panic!("No array"),
+	};
+	let toml = match toml[0] {
+		Value::Table(ref table) => table,
+		_ => panic!("No table"),
+	};
+	let toml = match toml["normals"] {
+		Value::Array(ref list) => list,
+		_ => panic!("No list"),
+	};
+	let mut vector = Vec::new();
+	for float in toml.chunks(3) {
+		let (one, two, three);
+		match float[0] {
+			Value::Float(value) => one = value as f32,
+			_ => panic!("Flerror"),
+		}
+		match float[1] {
+			Value::Float(value) => two = value as f32,
+			_ => panic!("Flerror"),
+		}
+		match float[2] {
+			Value::Float(value) => three = value as f32,
+			_ => panic!("Flerror"),
+		}
+		vector.push(Normal { normal: [one, two, three] });
+	}
+	VertexBuffer::new(display, &vector).unwrap()
+
+}
+
+fn create_face_buffer<F>(display: &F, table: &toml::Table) -> IndexBuffer<u32> where F: Facade {
+	use toml::Value;
+	let toml = match table["object"] {
+		Value::Array(ref array) => array,
+		_ => panic!("No array"),
+	};
+	let toml = match toml[0] {
+		Value::Table(ref table) => table,
+		_ => panic!("No table"),
+	};
+	let toml = match toml["faces"] {
+		Value::Array(ref list) => list,
+		_ => panic!("No list"),
+	};
+	let mut vector = Vec::new();
+	for float in toml.chunks(1) {
+		let one;
+		match float[0] {
+			Value::Integer(value) => one = value as u32,
+			_ => panic!("Flerror"),
+		}
+		vector.push(one);
+	}
+	IndexBuffer::new(display, glium::index::PrimitiveType::TrianglesList, &vector).unwrap()
+}
+
+fn create_vertex_buffer<F>(display: &F, table: &toml::Table) -> VertexBuffer<Vertex> where F: Facade {
 	use toml::Value;
 	let toml = match table["object"] {
 		Value::Array(ref array) => array,
@@ -61,5 +128,5 @@ fn create_vertex_buffer(display: &Facade, table: &toml::Table) -> VertexBuffer<f
 		}
 		vector.push(Vertex { position: [one, two, three] });
 	}
-	VertexBuffer::new(&display, &vector).unwrap()
+	VertexBuffer::new(display, &vector).unwrap()
 }
