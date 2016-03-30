f3787ac40d7bd293fd8786d4c7e2dc77e59e1254
Bourgond Aries <macocio@gmail.com>
2016-03-30 03:47:38 +0200

    Remove some source files that are unused

--- src/hello.rs
+++
@@ -1,9 +0,0 @@
-pub fn world(lhs: i64, op: &str, rhs: i64) -> Result<i64, &str> {
-	match op {
-		"+" => Ok(lhs + rhs),
-		"-" => Ok(lhs - rhs),
-		"*" => Ok(lhs * rhs),
-		"/" => Ok(lhs / rhs),
-		_ => Err("No suitable operator"),
-	}
-}
